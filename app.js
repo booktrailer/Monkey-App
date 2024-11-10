8742b639ac9a801a9b3113832607b928636ea889
Tierra Bots FLL 55496 <154866629+booktrailer@users.noreply.github.com>
2024-11-10 13:04:56 -0800

    added req

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-Test;
+const http = require('http');
+const dt = require('./date');
+const url = require('url');
+
+http.createServer(
+    function (req, res) {
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("the date is currently " + dt.find_date());
+  res.write(req.url)
+  res.end('Hello World!');
+
+    }
+).listen(8080);
