cf278ec7323a1bc0dd18d33df1add6082b71da9a
Tierra Bots FLL 55496 <154866629+booktrailer@users.noreply.github.com>
2024-11-16 22:01:03 -0800

    finally working!!! yay!!!

--- skib.js
+++ skib.js
@@ -1,4 +1,5 @@
 var times = [60, 180, 300];
+var timer_running = false
 
 function load_times() {
     
@@ -7,26 +8,33 @@ function load_times() {
         button_html += '<button id="button' + times[i].toString() + '" onclick="start_timer(' + times[i] + ')">' + (times[i]/60).toString() + ' minut</button>';
         }
     
-    var button_container = document.getElementById('button_container')
+    var button_container = document.getElementById('button_container');
     button_container.innerHTML = button_html;
 }
 
 function start_timer(time) {
-    var time_num = document.getElementById('time_num')
-    var curr_time = time;
-    time_num.innerHTML = toString(curr_time);
-    var curr_old_val = curr_time;
+    if (!(timer_running)) {
+        timer_running = true
+        console.log('started');
 
-    setInterval(function() {
-        curr_time--;
-    }, 1000);
+        var time_num = document.getElementById('time_num');
+        var curr_time = time;
+        time_num.innerHTML = String(curr_time);
+        var curr_old_val = curr_time;
 
-    while ( curr_time >= 0) {
-        if (curr_old_val != curr_time) {
-            curr_old_val = curr_time;
-            time_num.innerHTML = toString(curr_time);
-        }
+        console.log('gotpastpart1!!! yay');
 
-    time_num.innerHTML = 'times up!!!';
+        setInterval(function() {
+            curr_time--;
+            time_num.innerHTML = String(curr_time);
+
+            if (curr_time <= 0) {
+                clearInterval(timerInterval)
+                time_num.innerHTML = 'times up!!!';
+                timer_running = false
+            }
+        }, 1000);
+
     }
+
 }
