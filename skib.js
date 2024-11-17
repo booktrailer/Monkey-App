b45fd0a0acebfa9ec8aa37f8dd6d32fa39ee9138
Tierra Bots FLL 55496 <154866629+booktrailer@users.noreply.github.com>
2024-11-16 21:07:34 -0800

    overcomplicated button list that does not work

--- skib.js
+++ skib.js
@@ -1,11 +1,27 @@
 var times = [60, 180, 300];
+button_list = []
+button_elements = []
+button_list = document.getElementById('button_container');
 
 function load_times() {
+    
     var button_html = '';
     for (let i = 0; i < times.length; i++) {
-        button_html += '<button id="button' + times[i].toString() + '">' + (times[i]/60).toString() + '</button>';
+        if (!(button_list.includes('button' + times[i].toString()))) {
+        button_html += '<button id="button' + times[i].toString() + '">' + (times[i]/60).toString() + ' minut</button>';
+        button_list.push('button' + times[i].toString())
+
+        button_list.innerHTML = button_html;
+        button_elements.push(document.getElementById('button' + times[i].toString()));
+        }
     }
 
-    button_list = document.getElementById('button_container');
+    console.log(times)
+    console.log(button_list)
+    console.log(button_elements)
+
+    
+
+    
     button_list.innerHTML = button_html;
 }
