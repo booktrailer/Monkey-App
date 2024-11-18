6bf2708f2ae7fcf8e1acfb91940c085747332cab
Tierra Bots FLL 55496 <154866629+booktrailer@users.noreply.github.com>
2024-11-17 16:43:33 -0800

    add custum times per refresh and alarm cancelling

--- skib.js
+++ skib.js
@@ -1,12 +1,13 @@
 var times = [5, 60, 180, 300];
 var timer_running = false
 var audio = new Audio('lose-sound.mp3');
+var the_interval = null;
 
 function load_times() {
     
     var button_html = '';
     for (let i = 0; i < times.length; i++) {
-        button_html += '<button id="button' + times[i].toString() + '" onclick="start_timer(' + times[i] + ')">' + (times[i]/60).toString() + ' minut</button>';
+        button_html += '<button id="button' + times[i].toString() + '" onclick="start_timer(' + times[i] + ')">' + times[i].toString() + ' seconds' + '</button>';
         }
     
     var button_container = document.getElementById('button_container');
@@ -25,12 +26,12 @@ function start_timer(time) {
 
         console.log('gotpastpart1!!! yay');
 
-        var timerz = setInterval(function() {
+        the_interval = setInterval(function() {
             curr_time--;
             time_num.innerHTML = String(curr_time);
 
             if (curr_time <= 0) {
-                clearInterval(timerz);
+                clearInterval(the_interval);
                 time_num.innerHTML = 'times up!!!';
                 timer_running = false;
                 audio.play();
@@ -44,4 +45,23 @@ function start_timer(time) {
 function stop_audio() {
     audio.pause();
     audio.currentTime = 0;
+}
+
+function new_time() {
+    console.log('submitted');
+    custom_input = document.getElementById('custom_input');
+    console.log(custom_input.value)
+
+    if (custom_input.value > 0) {
+        console.log('greater')
+        times.push(custom_input.value);
+        console.log(times);
+        load_times();
+    }
+
+}
+
+function cancel() {
+    clearInterval(the_interval);
+    time_num.innerHTML = 'cancelled successfully';
 }
